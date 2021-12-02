c3e6196875f6e2a35103e0f94848e234a6b7e79b
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2021-12-02 11:44:24 +0000

    separate logic part and display part in components

--- src/App.js
+++ src/App.js
@@ -5,20 +5,35 @@ function Heading() {
 }
 
 function SongPlayer() {
+  const showControls = false || true;
+  const audioURL = "https://examples.devmastery.pl/assets/audio/deadfro5h.mp3";
   return (
     <>
       <Heading />
-      <audio controls>
-        <source src="https://examples.devmastery.pl/assets/audio/deadfro5h.mp3" />
+      <audio controls={showControls}>
+        <source src={audioURL} />
       </audio>
     </>
   );
 }
 
+function getStatusMessage(isLoading, hasErrors) {
+  let message = null;
+  if (isLoading) {
+    message = "Loading...";
+  }
+  if (hasErrors) {
+    message = "Errors occured...";
+  }
+  return message;
+}
+
 export default function App() {
+  const isLoading = false;
+  const hasErrors = false;
+  const showPlayer = !isLoading && !hasErrors;
+  const statusMessage = getStatusMessage(isLoading, hasErrors);
   return (
-    <div className="App">
-      <SongPlayer />
-    </div>
+    <div className="App">{showPlayer ? <SongPlayer /> : statusMessage}</div>
   );
 }
