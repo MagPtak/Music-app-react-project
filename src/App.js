f94341569d232aae8877f3313367b94f02fae886
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2021-12-03 12:34:18 +0000

    add useRef hook to SongPlayer component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./styles.css";
 
 function Heading({ title }) {
-  // Props przekazany jako argument funkcji
-  // const title = props.title;
   return <h1>{title}</h1>;
 }
 
 function SongPlayer({ showControls = true, song }) {
+  const audioRef = useRef();
   const { audioURL, coverURL } = song;
-  // const showControls = props.showControls;
-  // const audioURL = props.audioURL;
-  // DESTRUKTURYZACJA =>
-  // SKRÓCONA FORMA DESTRUKTURYZACJI - USUNIĘCIE KLUCZA jeśli nazwa klucza jest taka sama jak stałej
-  // const { showControls, audioURL } = props;
-  // const { showControls: showControls, audioURL: audioURL} = props;
 
   return (
     <>
       <Heading title="Music player" />
       <img width="250px" height="250px" src={coverURL} alt="Song" />
-      <audio key={audioURL} controls={showControls}>
+      <audio ref={audioRef} key={audioURL} controls={showControls}>
         <source src={audioURL} />
       </audio>
+      <div>
+        <button onClick={() => audioRef.current.play()}>Play</button>
+        <button onClick={() => audioRef.current.pause()}>Pause</button>
+      </div>
     </>
   );
 }
