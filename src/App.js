03bdfb1e9b21d358165816295e94e1fb61400d87
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2021-12-06 12:23:35 +0000

    add comments

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function Heading({ title }) {
   return <h1>{title}</h1>;
 }
 
+//useRef zostanie wykorzystany do przekazania referencji do obiektu i wywołania na nim odpowiedniej metody. Przez zmienną audioRef zostanie przekazana referencja do obiektu audio. Po kliknięciu w przycisk wywołuje się metodę na obiekcie audio, jest to molzliwe poprzez uzyskanie do niego referencji.
 function SongPlayer({ showControls = false, song }) {
   const audioRef = useRef();
   const { audioUrl, coverUrl } = song;
@@ -37,6 +38,8 @@ function SongListItem({ song, isCurrent, onSelect }) {
   );
 }
 
+//useState jest zainicjalizowany pustą tablicą, jest z niego wyciągana aktualna wartość, funckję do zmieniania wartości. Będzie tu przechowywana lista ściągniętych piosenek.
+//useEfect słuy do pobrania piosenek tylko raz, w przeciwnym wypadku wykonywałby się tyle razy ile funkcja, przy kazdej zmianie piosenki. Jako pierwszy argument przyjmuje funkcję wywołującą efekt uboczny czyli pobranie listy piosenek, drugim argumentem jest pusta tablica, która powoduje ze  piosenki zostaną pobrane tylko raz po pierwszym renderze.
 export default function App() {
   const URL = "https://examples.devmastery.pl/songs-api/songs";
   const [songs, setSongs] = useState([]);
@@ -50,6 +53,7 @@ export default function App() {
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const currentSong = songs[currentSongIndex];
 
+  //Funkcja wybierająca aktualną piosenkę -> stała audioIndex słuy do iterowania tablicy songs ze wszystkimi piosenkami, sprawdzenia ich indeków i porównania ich audioUrl z audioUrl wybranej piosenki, funkcja zwróci indeks piosenki, następnie wywoła funkcję setCurrentSongIndex i przekaze do niej audioIndex
   function handleSelectSong(selectedSong) {
     const audioIndex = songs.findIndex(
       (song) => song.audioUrl === selectedSong.audioUrl
